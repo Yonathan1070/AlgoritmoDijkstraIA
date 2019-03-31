da6d1b50fec39067ffa3261f919aefc04114f58a
Yonathan <Yonathan@DESKTOP-7B9VFVK>
2019-03-30 19:06:19 -0500

    Nodos organizados

--- src/main/java/com/mycompany/ejemplodijkstra/Ejemplos.java
+++ src/main/java/com/mycompany/ejemplodijkstra/Ejemplos.java
@@ -59,35 +59,35 @@ public class Ejemplos {
     
     public char[] GrafoLaberinto(Grafo g){
         g.agregarRuta('a','b', 15);
+        g.agregarRuta('a','n', 0);
+        g.agregarRuta('a','i', 0);
         g.agregarRuta('b','a', 15);
         g.agregarRuta('b','ñ', 16);
-        g.agregarRuta('ñ','b', 16);
         g.agregarRuta('b','c', 17);
         g.agregarRuta('c','b', 17);
-        g.agregarRuta('e','f', 37);
-        g.agregarRuta('f','e', 37);
-        g.agregarRuta('e','d', 60);
         g.agregarRuta('d','e', 60);
+        g.agregarRuta('e','f', 37);
+        g.agregarRuta('e','d', 60);
         g.agregarRuta('e','g', 27);
+        g.agregarRuta('f','e', 37);
         g.agregarRuta('g','e', 27);
-        g.agregarRuta('a','n', 0);
-        g.agregarRuta('n','a', 0);
-        g.agregarRuta('a','i', 0);
-        g.agregarRuta('i','a', 0);
         g.agregarRuta('g','n', 5);
-        g.agregarRuta('n','g', 5);
         g.agregarRuta('g','h', 10);
-        g.agregarRuta('h','g', 10);
-        g.agregarRuta('h','i', 8);
-        g.agregarRuta('i','h', 8);
         g.agregarRuta('g','k', 70);
-        g.agregarRuta('k','g', 70);
+        g.agregarRuta('h','g', 10);
         g.agregarRuta('h','j', 80);
+        g.agregarRuta('h','i', 8);
+        g.agregarRuta('i','a', 0);
+        g.agregarRuta('i','h', 8);
         g.agregarRuta('j','h', 80);
+        g.agregarRuta('k','g', 70);
         g.agregarRuta('k','m', 5);
-        g.agregarRuta('m','k', 5);
         g.agregarRuta('k','l', 70);
         g.agregarRuta('l','k', 70);
+        g.agregarRuta('m','k', 5);
+        g.agregarRuta('n','a', 0);
+        g.agregarRuta('n','g', 5);
+        g.agregarRuta('ñ','b', 16);
         
         String dato = "al";
         
